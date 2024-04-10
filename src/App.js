bff3737d83556d217cae4b9286dbef869a79728c
sanv1zl <sanva970@gmail.com>
2024-04-10 23:22:11 +0300

    moved MainComponent into components folder and added margin to button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import AllIconsComponent from './MainComponent'; 
+import AllIconsComponent from './components/MainComponent';
 
 function App() {
   return (
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+import App from '../App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
